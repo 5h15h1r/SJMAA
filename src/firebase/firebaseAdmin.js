9f7ff10c3ad78f825379a54cab39d4550d0f66fd
5h15h1r <shishir.agrahari14@gmail.com>
2023-08-30 19:20:32 +0530

    fixed errors

---
+++ src/firebase/firebaseAdmin.js
@@ -0,0 +1,16 @@
+// import { getAuth } from 'firebase/auth';
+
+
+// const firebaseConfig = {
+//     apiKey: "AIzaSyABmpXc_sXgcLYaxV6bjz4MWAWKqCQVtNg",
+//     authDomain: "alumni-4661c.firebaseapp.com",
+//     projectId: "alumni-4661c",
+//     storageBucket: "alumni-4661c.appspot.com",
+//     messagingSenderId: "532780708697",
+//     appId: "1:532780708697:web:dae637c0ca045a33669248",
+//     measurementId: "G-1W7T9H9D85"
+//   };
+
+//   const adminApp = initializeApp(firebaseConfig);
+
+// export default adminApp
