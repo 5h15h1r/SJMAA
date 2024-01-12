549f2d61f8767cfe2cae240a03a3c94b2db1e3b3
arjun3649 <arjun.rakwal3649@gmail.com>
2024-01-13 00:34:58 +0530

    Added Newsroom

---
+++ src/components/NewsroomComponent.js
@@ -0,0 +1,30 @@
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable @next/next/no-img-element */
+import React from "react";
+
+const NewsroomComponent = ({
+  image = "",
+  heading = "",
+  text = "",
+  buttontext = "",
+  
+  className= "",
+}) => {
+  return (
+    <div
+      className={` flex flex-row w-[70%] h-[200px] bg-slate-100 mt-2 mb-2 ${className}`}
+    >
+      <div>
+        <img src={image} height="100px" width="180px" className="ml-1 mt-1 mb-1" alt="img" ></img>
+      </div>
+      <div className="flex flex-col ml-4">
+        <h1 className="font-bold font-sans">{heading}</h1>
+        <p>date of posting</p>
+        <p>{text}</p>
+        <button className="rounded-lg w-[100px] h-[25px] bg-slate-500 mt-[30px]"> {buttontext}</button>
+      </div>
+    </div>
+  );
+};
+
+export default NewsroomComponent;
--- src/pages/page/newsroom.js
+++ src/pages/page/newsroom.js
@@ -1,12 +1,73 @@
-import Navbar from '@/components/Navbar'
-import React from 'react'
+/* eslint-disable react/no-unescaped-entities */
+import Navbar from "@/components/Navbar";
+import React from "react";
+import NewsroomComponent from "@/components/NewsroomComponent";
 
 const newsroom = () => {
   return (
-    <div>
+    <div div className="bg-slate-300 h-[150vh]">
+      <Navbar />
+      <div className="flex flex-col justify-center items-center ml-[90px] bg-white mt-2 mb-[30px] w-[60%] h-auto shadow-2xl rounded-lg">
+        <NewsroomComponent
+        
+          image={
+           " https://vaave.s3.amazonaws.com/content/151669119_1669962012_toppersICSEISC2020_thumb.jpg"
+          }
+          heading={"Toppers list of ICSE 2020 Examinations"}
+          text={"Congratulations to all the Toppers!"}
+          buttontext={"Read more"}
+        />
+        <NewsroomComponent
+          image={
+            "https://vaave.s3.amazonaws.com/content/151669119_1669961862_Scan-01-Feb-2021-1_thumb.jpg"
+          }
+          
+          heading={"Fee Structure"}
+          text={"The fee structure details are mentioned in the image"}
+          buttontext={"Read more"}
+        />
+        <NewsroomComponent
+        
+          image={
+           " https://vaave.s3.amazonaws.com/content/151669119_1669962012_toppersICSEISC2020_thumb.jpg"
+          }
+          heading={"Toppers list of ICSE 2020 Examinations"}
+          text={"Congratulations to all the Toppers!"}
+          buttontext={"Read more"}
+        />
+        <NewsroomComponent
+          image={
+            "https://vaave.s3.amazonaws.com/content/151669119_1669961862_Scan-01-Feb-2021-1_thumb.jpg"
+          }
+          
+          heading={"Fee Structure"}
+          text={"The fee structure details are mentioned in the image"}
+          buttontext={"Read more"}
+        />
       
-    </div>
-  )
-}
+      </div>
+        <div>
+          <a href="https://www.google.com" className="no-underline ml-[90px]">St. John's School, Marhauli</a>
+          <a href="https://www.facebook.com" className="no-underline ml-[20px]">Facebook</a>
+          <a href="https://www.instagram.com" className="no-underline ml-[20px]">instagram</a>
+        </div>
+        <div className="flex flex-row h-[80px]  bg-gradient-to-r from-blue-700 via-blue-800 to-gray-900">
+          <div className="ml-[90px] mt-5">
+            <p>Powered by</p>
+            <h1 className="font-bold font-mono text-2xl text-white">Mera Lund</h1>
+          </div>
+          <div className="mt-[45px]">
+            <a href="" className="no-underline ml-[20px]">Copyright © 2024</a>
+            <a href="" className="no-underline ml-[20px]">Disclaimer </a>
+            <a href="" className="no-underline ml-[20px]">Terms of Use</a>
+            <a href="" className="no-underline ml-[20px]">Privacy Policy</a>
+            <a href="" className="no-underline ml-[20px]">Alumni Directory</a>
+          </div>
 
-export default newsroom
+
+        </div>
+    </div>
+  );
+};
+
+export default newsroom;
