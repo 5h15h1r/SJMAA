1a01cfc0ffebd45543e817f7fed80bfa035deeea
Shishir Agrahari <shishirrocks1420@gmail.com>
2024-01-20 13:30:20 +0530

    Responsive Newsroom

--- src/components/NewsroomComponent.js
+++ src/components/NewsroomComponent.js
@@ -1,30 +1,34 @@
 /* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable @next/next/no-img-element */
+import Image from "next/image";
 import React from "react";
 
 const NewsroomComponent = ({
-  image = "",
-  heading = "",
-  text = "",
-  buttontext = "",
-  
-  className= "",
+	image = "",
+	heading = "",
+	text = "",
+	buttontext = "",
+
+	className = "",
 }) => {
-  return (
-    <div
-      className={` flex flex-row w-[70%] h-[200px] bg-slate-100 mt-2 mb-2 ${className}`}
-    >
-      <div>
-        <img src={image} height="100px" width="180px" className="ml-1 mt-1 mb-1" alt="img" ></img>
+    return(
+      <div className="bg-slate-100 w-[90%] md:w-[60%`] p-4">
+          <div className="flex space-x-5 w-full md:w-">
+            <div className="w-[50%] md:w-auto">
+              {/* Image */}
+              <img height="" width="" src={image} className="object-" />
+            </div>
+
+            <div className="w-[50%] md:w-auto">
+              <h2 className="font-bold font-sans text-2xl">{heading}</h2>
+              <p>
+                {text}
+              </p>
+              <button className="px-4 py-2 rounded-3xl ml-auto bg-sky-200">Read more</button>
+            </div>
+          </div>
       </div>
-      <div className="flex flex-col ml-10">
-        <h1 className="font-bold font-sans">{heading}</h1>
-        <p>date of posting</p>
-        <p>{text}</p>
-        <button className="rounded-lg w-[100px] h-[25px] bg-slate-500 mt-[30px]"> {buttontext}</button>
-      </div>
-    </div>
-  );
+    )
 };
 
 export default NewsroomComponent;
--- src/pages/page/newsroom.js
+++ src/pages/page/newsroom.js
@@ -2,12 +2,13 @@
 import Navbar from "@/components/Navbar";
 import React from "react";
 import NewsroomComponent from "@/components/NewsroomComponent";
+import Footer from "@/components/Footer";
 
 const newsroom = () => {
   return (
     <div className="bg-slate-300 h-[150vh]">
       <Navbar />
-      <div className="flex flex-col justify-center items-center ml-10  bg-white mt-2 mb-[30px] w-[90%] md:w m  h-auto shadow-2xl rounded-lg">
+      <div className="w-full flex flex-col space-y-4  my-10 items-center justify-center max-w-6xl  shadow-2xl rounded-lg">
         <NewsroomComponent
         
           image={
@@ -46,26 +47,8 @@ const newsroom = () => {
         />
       
       </div>
-        <div>
-          <a href="https://www.google.com" className="no-underline ml-[90px]">St. John's School, Marhauli</a>
-          <a href="https://www.facebook.com" className="no-underline ml-[20px]">Facebook</a>
-          <a href="https://www.instagram.com" className="no-underline ml-[20px]">instagram</a>
-        </div>
-        <div className="flex flex-row h-[80px]  bg-gradient-to-r from-blue-700 via-blue-800 to-gray-900">
-          <div className="ml-[90px] mt-5">
-            <p>Powered by</p>
-            <h1 className="font-bold font-mono text-2xl text-white">Mera Lund</h1>
-          </div>
-          <div className="mt-[45px]">
-            <a href="" className="no-underline ml-[20px]">Copyright © 2024</a>
-            <a href="" className="no-underline ml-[20px]">Disclaimer </a>
-            <a href="" className="no-underline ml-[20px]">Terms of Use</a>
-            <a href="" className="no-underline ml-[20px]">Privacy Policy</a>
-            <a href="" className="no-underline ml-[20px]">Alumni Directory</a>
-          </div>
-
-
-        </div>
+       
+       <Footer />
     </div>
   );
 };
