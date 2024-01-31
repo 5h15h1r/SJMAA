893412af4671dbbd96af1babeaf1d332d35e8095
arjun3649 <arjun.rakwal3649@gmail.com>
2024-02-01 03:54:38 +0530

    Added gallery component

--- src/pages/page/gallery.js
+++ src/pages/page/gallery.js
@@ -2,25 +2,27 @@ import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 import Image from "next/image";
 import React from "react";
+import  { useRouter } from "next/router";
 
 const gallery = () => {
+  const router=useRouter()
   return (
     <div className="bg-white h-[100vh] w-full">
       <Navbar />
       <div className=" flex flex-row w-[90%] md:max-w-6xl bg-[#355245] h-[50px] ml-[5%] mt-10 rounded-lg shadow-2xl">
-        <a href="#" className="p-1 mx-2 my-2">
+        <a href="/page/gallery/album" className="p-1 mx-2 my-2" >
           Album
         </a>
 
-        <a href="#" className="p-1 mx-2 my-2">
+        <a href="/page/gallery/videos" className="p-1 mx-2 my-2">
           Videos
         </a>
-        <a href="#" className="p-1 mx-2 my-2">
+        <a href="/page/gallery/memories" className="p-1 mx-2 my-2">
           Memories
         </a>
       </div>
       <div className="flex flex-row h-[450px] bg-[#355245] w-[90%] ml-[5%] mt-10 md:max-w-6xl rounded-lg shadow-2xl ">
-        <div className="mx-4 my-4 bg-[#648374] h-[200px] w-[200px] cursor-pointer " onClick={()=>{console.log("clciked")}}>
+        <div className="mx-4 my-4 bg-[#648374] h-[200px] w-[200px] cursor-pointer " onClick={()=>{console.log("")}}>
           <img
             className="h-full w-full object-cover"
             src="https://i.pinimg.com/736x/c1/d1/bb/c1d1bbda5fb1f84d5beaf7ccea0e1cd0.jpg"
